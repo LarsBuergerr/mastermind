d225fb34d60f84eff6535651b0f6359e5678a094
Lars Bürger <lars.buerger@freenet.de>
2022-05-12 21:11:54 +0200

    Implemented a small test program

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,43 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import scala.util.Success
+import scala.util.Try
+import scala.util.Failure
+import scala.io.StdIn.readLine
 
-def msg = "I was compiled by Scala 3. :)"
+def RandomNumberGenerator(): Int =
+  
+
+  val r = new scala.util.Random
+  
+  return r.nextInt(100)
+
+def Loop():Unit =
+
+  val randomnr = RandomNumberGenerator()
+
+  print("Your guess: ")
+
+  val input = readLine(" ").toInt
+
+  if(randomnr == input)
+    print("\nCongratulations you guessed the correct number\n")
+  else
+    print("Sorry you picked the wrong number, the correct number was " + randomnr +"\nWanna try again? (y/n)\n")
+
+  val ans = readLine(">> ")
+
+  if(ans != "n")
+    return Loop()
+
+@main def main: Unit = 
+
+  print("Random Number between 0 and 100 is getting generated... \n")
+  Loop()
+
+  print("Thank you for playing our game!!!\n")
+
+
+
+
+
+
+
