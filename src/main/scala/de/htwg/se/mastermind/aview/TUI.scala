db0fadc8b78a794313759cf42dcf4f1ea2c94aa9
lars buerger <larsbuerger1@gmail.com>
2022-09-30 11:13:25 +0200

    Improved error handling in TUI

--- src/main/scala/de/htwg/se/mastermind/aview/TUI.scala
+++ src/main/scala/de/htwg/se/mastermind/aview/TUI.scala
@@ -7,6 +7,8 @@ import model.{Code, Field, Stone, HintStone}
 import scala.io.StdIn.readLine
 
 case class TUI(var controller: Controller) extends Observer:
+  val EXIT_VAL    = 1
+  val ERROR_VAL   = -1
   val SUCCESS_VAL = 0
   val loopCount = 0
   val code = new Code(controller.field.cols)
@@ -22,15 +24,37 @@ case class TUI(var controller: Controller) extends Observer:
     val newLoopCount = loopCount + 1
     val input = readLine(">> ")
     parseInput(input, loopCount) match {
-      case SUCCESS_VAL => {
+      case SUCCESS_VAL =>
         run(newLoopCount)
-      }
+
+      case ERROR_VAL   =>
+        run(loopCount)
+
+      case EXIT_VAL    =>
+        print("Exiting...\n")
     }
 
 
   def parseInput(input: String, loopCount: Int): Int =
     val emptyVector: Vector[Stone] = Vector()
     val chars = input.toCharArray()
+
+    if(chars.size == 0)
+      print("No input!\n")
+      return ERROR_VAL
+
+    if(chars.size == 1)
+      chars(0) match {
+        case 'h' | 'H' =>
+          printHelp()
+          return ERROR_VAL
+        case 'q' | 'Q' =>
+          return EXIT_VAL
+      }
+
+    if(chars.size != controller.field.matrix.cols)
+      print("Selected Code has the wrong length!\n")
+      return ERROR_VAL
 
     val codeVector    = buildVector(emptyVector, chars)
     val hints         = code.compareTo(codeVector)
@@ -51,6 +75,10 @@ case class TUI(var controller: Controller) extends Observer:
         buildVector(newvector, chars)
       else
         return newvector
+  
+  def printHelp() =
+    println("Userinput example at Codelength 4: 'rgby' would indicate a Code with the Colors Red, Green, Blue, Yellow\n")
 
+  
   override def update: Unit = 
     println(controller.update)
