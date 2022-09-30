f098a56f21ef177b1d18716e238dd9d56d1d3152
Smokey95 <simon.htwg@gmail.com>
2022-09-30 10:45:01 +0200

    Change Stone Object to see if coverage increase

--- src/main/scala/de/htwg/se/mastermind/model/Stone.scala
+++ src/main/scala/de/htwg/se/mastermind/model/Stone.scala
@@ -9,7 +9,11 @@ package model
 //********************************************************************** IMPORTS
 import scala.util.Random
 
-/* Enum declaration for the mastermind stones */
+/**
+  * Enum declaration for the mastermind game stones
+  *
+  * @param stringRepresentation
+  */
 enum Stone(stringRepresentation: String):
   override def toString(): String = stringRepresentation
   case Red extends Stone("R")
@@ -20,14 +24,24 @@ enum Stone(stringRepresentation: String):
   case Purple extends Stone("P")
   case Empty extends Stone(" ")
 
-/* 
- *Object declaration for random Stone function
- * @IMPORTANT: size - 1 to avoid empty stones in random generated codes
- */
-object Stone:
-  private final val color = Stone.values
-  def random: Stone = color(Random.nextInt(color.size - 1))
   
+/**
+  * Object declaration for random Stone function
+  * @Important: size - 1 to avoid empty stones in random generated codes
+  */
+object Stone:
+  val color = Stone.values
+  
+  def random: Stone = {
+    color(Random.nextInt(color.size - 1))
+  }
+
+  
+/**
+  * Enum declaration for the mastermind hint stones (Black and White)
+  *
+  * @param stringRepresentation
+  */  
 enum HintStone(stringRepresentation: String):
   override def toString(): String = stringRepresentation
   case Black extends HintStone("B")
