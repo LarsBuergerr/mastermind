4d7f0d3d611c86f3b47d30a3b8311af0c0c5963b
lars buerger <larsbuerger@gmail.com>
2022-10-04 15:36:57 +0200

    Implemented simple state pattern

--- src/main/scala/de/htwg/se/mastermind/Main.scala
+++ src/main/scala/de/htwg/se/mastermind/Main.scala
@@ -1,8 +1,9 @@
 package de.htwg.se.mastermind
 import aview.TUI
+import model.GameMode
 
 @main def main: Unit = 
-
-  print("Welcome to Mastermind\n")
-  val tui = new TUI()
+  print("Welcome to Mastermind\n\n")
+  val mode = GameMode.mode
+  val tui = new TUI(mode)
   tui.run(0)
---
+++ src/main/scala/de/htwg/se/mastermind/model/GameMode.scala
@@ -0,0 +1,31 @@
+package de.htwg.se.mastermind
+
+package model
+
+import controller.Controller
+import scala.io.StdIn.readLine
+
+object GameMode {
+
+  var mode = parseInput()
+
+  def easy = new Controller(new Field(12, 4, Stone.Empty, HintStone.Empty))
+
+  def medium = new Controller(new Field(10, 4, Stone.Empty, HintStone.Empty))
+
+  def hard = new Controller(new Field(10, 5, Stone.Empty, HintStone.Empty))
+
+  def extrem = new Controller(new Field(8, 5, Stone.Empty, HintStone.Empty))
+
+  def parseInput(): Controller =
+   val in = readLine("Choose gamemode between [Easy, Medium, Hard, Extrem], default= Medium: ")
+
+   in match {
+     case "Easy" | "easy"        => return easy
+     case "Medium" | "medium"    => return medium
+     case "Hard" | "hard"        => return hard
+     case "Extrem" | "extrem"    => return extrem
+     case _                      => return medium
+   }
+}
+
