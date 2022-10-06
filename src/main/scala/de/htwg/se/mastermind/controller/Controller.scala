fe3fcab7a22a0bc4bac3db6d2c33dae42317b300
Smokey95 <simon.htwg@gmail.com>
2022-10-06 10:17:04 +0200

    Push current version for lars

--- src/main/scala/de/htwg/se/mastermind/controller/Controller.scala
+++ src/main/scala/de/htwg/se/mastermind/controller/Controller.scala
@@ -2,12 +2,13 @@ package de.htwg.se.mastermind
 package controller
 
 import model.{Field, Stone, HintStone}
-import util.Observable
+import util.*
 
 class Controller(var field: Field) extends Observable:
 
     def this() =
         this(new Field())
+        
 
     def placeGuessAndHints(stone: Vector[Stone],hints: Vector[HintStone], row: Int): Unit =
         field = field.placeGuessAndHints(stone, hints, row)
--- src/main/scala/de/htwg/se/mastermind/util/GameState.scala
+++ src/main/scala/de/htwg/se/mastermind/util/GameState.scala
@@ -1,51 +1,58 @@
+/**
+  * GameState.scala
+  * Implements the STATE_PATTERN to keep track over current game state
+  */
+
+//********************************************************************** PACKAGE  
 package de.htwg.se.mastermind
 package util
 
-import scala.io.StdIn.readLine
 
-trait Event
+trait GameState
 
-case class MenuState()  extends Event
-case class PlayState()  extends Event
-case class IntroState() extends Event
+case class MenuState() extends GameState
+case class PlayState() extends GameState
+case class InitState() extends GameState
 
-object GameState{
-  
-  var state = introState
-  
-  def handle(event: Event) = {
-    
-    event match{
-      case intro:   IntroState => state = introState
-      
-      case menu:    MenuState => state = menuState
-      
-      case play:    PlayState => state = playState
-    }
-    state
-  }
-  
-  def introState = {
-    println("----------------------------------------------------------------")
-    println("------------------ Welcome to Mastermind -----------------------")
-    println("----------------------------------------------------------------")
-  }
-  
-  def menuState = {
-    println("----------------------------------------------------------------")
-    println("Mastermind Menu:")
-    println("- gamemode | Change Gamemode")
-    val input = readLine(">> ")
-    
-    input match {
-      case "gamemode" => 
-        GameMode
-    }
-  }
-  
-  def playState = {
-    println("Lets play a round, can you guess the code?")
-    Controller.field.toString
-  }
-  
-}
+case class EndState() extends GameState
+
+//object GameState{
+//  
+//  var state = introState
+//  
+//  def handle(event: Event) = {
+//    
+//    event match{
+//      case intro:   IntroState => state = introState
+//      
+//      case menu:    MenuState => state = menuState
+//      
+//      case play:    PlayState => state = playState
+//    }
+//    state
+//  }
+//  
+//  def introState = {
+//    println("----------------------------------------------------------------")
+//    println("------------------ Welcome to Mastermind -----------------------")
+//    println("----------------------------------------------------------------")
+//  }
+//  
+//  def menuState = {
+//    println("----------------------------------------------------------------")
+//    println("Mastermind Menu:")
+//    println("- gamemode | Change Gamemode")
+//    val input = readLine(">> ")
+//    
+//    input match {
+//      case "gamemode" => 
+//        GameMode
+//    }
+//  }
+//  
+//  def playState = {
+//    println("Lets play a round, can you guess the code?")
+//    Controller.field.toString
+//  }
+//  
+//}
