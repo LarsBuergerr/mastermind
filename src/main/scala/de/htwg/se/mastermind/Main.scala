1dcc425b3c575478faa75bed495037d8288cdd93
Smokey95 <simon.publicmail@gmail.com>
2022-10-12 20:47:29 +0200

    Compress main class to a one liner

--- src/main/scala/de/htwg/se/mastermind/Main.scala
+++ src/main/scala/de/htwg/se/mastermind/Main.scala
@@ -9,11 +9,9 @@ package de.htwg.se.mastermind
 //********************************************************************** IMPORTS
 import aview.TUI
 import controller.Controller
-import model.{Game, State, Init}
-import util.{GameMode}
+import model.Game
+import util.GameMode
 
-@main def main: Unit = 
-  val game = new Game(GameMode.selectMode)
-  val controller = new Controller(game)
-  val tui = new TUI(controller)
-  tui.run()
+
+//******************************************************************** CLASS DEF
+@main def main: Unit = new TUI(new Controller(new Game(GameMode.selectMode))).run()
