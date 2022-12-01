d3fa5eb53d2657651c0ca4ad3cdeee7526e17d28
Smokey95 <simon.htwg@gmail.com>
2022-12-01 16:35:01 +0100

    Add Logo and test some stuff

--- src/main/scala/de/htwg/se/mastermind/Main.scala
+++ src/main/scala/de/htwg/se/mastermind/Main.scala
@@ -10,16 +10,32 @@ package de.htwg.se.mastermind
 import aview.{TUI, GUI}
 import controller.Controller
 import model.Game
-import util.GameMode
+import util.GameMode._
 import scalafx.application.JFXApp3
+import de.htwg.se.mastermind.util.GameMode
 
+
+
+//******************************************************************** CLASS DEF
 
 object starter {
+  
+  val game = Game(GameMode.strategy_medium)
+  val controller = Controller(game)
+  
   def runGUI = {
     GUI().start()
   }
+  
+  def runTUI = {
+    TUI(controller).run()
+  }
 }
-//******************************************************************** CLASS DEF
+
+object MainTUI extends App {
+  starter.runTUI
+}
+
 object MainGUI extends JFXApp3 {
   override def start() =
     starter.runGUI
--- src/main/scala/de/htwg/se/mastermind/aview/GUI.scala
+++ src/main/scala/de/htwg/se/mastermind/aview/GUI.scala
@@ -26,16 +26,20 @@ class GUI() extends JFXApp3 {
     
     override def start() = {
         stage = new JFXApp3.PrimaryStage {
+            icons += new Image(getClass.getResource("/logo.png").toExternalForm, 100, 100, true, true)
+            resizable = false
             title.value = "Mastermind"
 
-            scene = new Scene(1000, 1000) {
+            scene = new Scene(400, 800) {
+                this.setCursor(new ImageCursor(new Image(getClass.getResource("/mouse.png").toExternalForm, 100, 100, true, true))) 
+                
                 val border = new BorderPane()
                 val pane = new GridPane()
                 pane.setMaxWidth(300)
                 pane.setMaxHeight(300)
                 pane.setAlignment(CENTER)
 
-                for (i <- 0 to 9) {
+                for (i <- 0 to 3) {
                     for (j <- 0 to 9) {
                         val entry = new Entry(i, j)
                         pane.add(entry, i, j)
@@ -45,23 +49,28 @@ class GUI() extends JFXApp3 {
 
                 root = border
             }
+          
         }
     }
 
 
     class Entry(x: Int, y: Int) extends StackPane {
+      
+        val circle_size = 80
+        val image_size = circle_size - 5
 
-        this.setMinSize(30, 30)
-        this.setPrefSize(30, 30)
-        this.setMaxSize(30, 30)
+        this.setMinSize(circle_size, circle_size)
+        this.setPrefSize(circle_size, circle_size)
+        this.setMaxSize(circle_size, circle_size)
         val label = new Label("")
-        label.setPrefSize(30, 30)
-        label.setMinSize(30, 30)
-        label.setMaxSize(30, 30)
-        label.setGraphic(new ImageView(new Image("blue_square.png")))
+        label.setPrefSize(circle_size, circle_size)
+        label.setMinSize(circle_size, circle_size)
+        label.setMaxSize(circle_size, circle_size)
+        label.setGraphic(new ImageView(new Image("circle_back_dark_512.png", image_size, image_size, smooth = true, preserveRatio = true)))
         
-        label.setOnMouseEntered(e => label.setGraphic(new ImageView(new Image("light_blue_square.png"))))
-        label.setOnMouseExited(e => label.setGraphic(new ImageView(new Image("blue_square.png"))))
+        label.setOnMouseEntered(e => label.setGraphic(new ImageView(new Image("circle_back_light_512.png", image_size, image_size, true, true))))
+        label.setOnMouseExited(e => label.setGraphic(new ImageView(new Image("circle_back_dark_512.png", image_size, image_size, true, true))))
+        label.setOnMouseClicked(e => label.setGraphic(new ImageView(new Image("test.gif", image_size, image_size, true, true))))
 
         this.getChildren().add(label)
     }
