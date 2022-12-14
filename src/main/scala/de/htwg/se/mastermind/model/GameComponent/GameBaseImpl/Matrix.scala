2e8c71f1ccff2ba606e52305a848d47795362efa
Smokey95 <simon.htwg@gmail.com>
2022-12-14 16:25:14 +0100

    Was ist das? Das ist Schnaps

--- src/main/scala/de/htwg/se/mastermind/model/GameComponent/GameBaseImpl/Matrix.scala
+++ src/main/scala/de/htwg/se/mastermind/model/GameComponent/GameBaseImpl/Matrix.scala
@@ -2,28 +2,31 @@
   * Matrix.scala
   */
 
-//********************************************************************** PACKAGE
+//****************************************************************************** PACKAGE
 package de.htwg.se.mastermind
 package model
 
 package GameComponent
 package GameBaseImpl
 
-//********************************************************************** IMPORTS
+//****************************************************************************** IMPORTS
 
 
-/**
-  * 
-  *
-  * @param m
-  */
+//****************************************************************************** CLASS DEFINITION
 case class Matrix[T](m: Vector[Vector[T]]):
+  
   def this(rows: Int,cols: Int, filling: T) = this(Vector.tabulate(rows, cols) { (row, col) => filling })
+  
   val rows: Int = m.size
+  
   val cols: Int = m.head.size
 
   def cell(row: Int, col: Int): T = m(row)(col)
+  
   def row(row: Int) = m(row)
+  
   def fill(filling: T): Matrix[T] = copy(Vector.tabulate(rows, cols) { (row, col) => filling })
+  
   def replaceCell(row: Int, col: Int, cell: T): Matrix[T] = copy(m.updated(row, m(row).updated(col, cell)))
+  
   def replaceRow(row: Int, vec: Vector[T]): Matrix[T] = copy(m.updated(row, vec))
