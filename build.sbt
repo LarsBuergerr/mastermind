0431d3672edfe0a5550dc596661f58e645a8e92c
lars buerger <larsbuerger1@gmail.com>
2022-12-27 12:39:55 +0100

    json impl of FileIO

--- build.sbt
+++ build.sbt
@@ -21,7 +21,8 @@ lazy val root = project
       ("net.codingwell" %% "scala-guice" % "5.0.2").cross(CrossVersion.for3Use2_13),
       "org.scala-lang.modules" %% "scala-xml" % "2.0.1", // XML
       "com.lihaoyi" %% "upickle" % "1.4.4", // JSON
-      ("net.jcazevedo" %% "moultingyaml" % "0.4.2").cross(CrossVersion.for3Use2_13)), //YAML
+      ("net.jcazevedo" %% "moultingyaml" % "0.4.2").cross(CrossVersion.for3Use2_13), //YAML
+      ("com.typesafe.play" %% "play-json" % "2.9.3").cross(CrossVersion.for3Use2_13)), // JSON
     },
     libraryDependencies ++= {
   // Determine OS version of JavaFX binaries
--- src/main/scala/de/htwg/se/mastermind/model/FileIOComponent/fileIOJsonImpl/FileIO.scala
+++ src/main/scala/de/htwg/se/mastermind/model/FileIOComponent/fileIOJsonImpl/FileIO.scala
@@ -5,9 +5,98 @@ package FileIOComponent
 package fileIOJsonImpl
 
 import GameComponent.GameInterface
-import GameComponent.GameBaseImpl.{Field, Stone, Matrix}
+import GameComponent.GameBaseImpl.{Field, Stone, Matrix, HStone, HintStone, Code, Game}
+
+//import json lib
+import play.api.libs.json.*
 
 class FileIO extends FileIOInterface {
-  override def load: GameInterface = ???
-  override def save(game: GameInterface): Unit = ???
-}
+  override def load: GameInterface = 
+    import scala.io.Source
+    val source: String = Source.fromFile("game.json").getLines.mkString
+    val json: JsValue = Json.parse(source)
+    JsonToGame(json)
+    
+  
+  override def save(game: GameInterface): Unit = 
+    import java.io._
+    import scala.xml._
+    val pw = new PrintWriter(new File("game.json"))
+    pw.write(gameToJson(game).toString())
+    pw.close()
+
+  def cellToJson(cell: Object, x: Int, y: Int) = 
+    val cellJson = Json.obj(
+      "x" -> x,
+      "y" -> y,
+      "value" -> cell.toString()
+    )
+    cellJson
+
+  def vectorToJson(vector: Vector[Object], row: Int) =
+    Json.obj(
+      "row" -> row,
+      "cells" -> {
+        //for loop with index
+        for (i <- vector.indices) yield cellToJson(vector(i), row, i)
+      }
+    )
+
+
+  def matrixToJson(matrix: Matrix[Object]) =
+    Json.obj(
+      "matrix" -> {
+        for (row <- matrix.m.indices) yield vectorToJson(matrix.m(row), row)
+      }
+    )
+
+
+  def gameToJson(game: GameInterface) =
+    Json.obj(
+      "matrix" -> {
+        for (row <- game.field.matrix.m.indices) 
+          yield vectorToJson(game.field.matrix.m(row), row)
+      },
+      "hmatrix" -> {
+        for (row <- game.field.hmatrix.m.indices) 
+          yield vectorToJson(game.field.hmatrix.m(row), row)
+      },
+      "turn" -> game.currentTurn,
+      "code" -> vectorToJson(game.code.code.asInstanceOf[Vector[Object]], 0),
+    )
+  }
+
+  def JsonToStone(cellJson: JsValue) = 
+  val x = cellJson("x").as[Int]
+  val y = cellJson("y").as[Int]
+  val value = cellJson("value").as[String]
+  val cell = Stone(value)
+  cell
+
+def JsonToHStone(cellJson: JsValue) = 
+  val x = cellJson("x").as[Int]
+  val y = cellJson("y").as[Int]
+  val value = cellJson("value").as[String]
+  val cell = HintStone(value)
+  cell
+
+
+def JsonToVector(vectorJson: JsValue, mtype: String) =
+  val row = vectorJson("row").as[Int]
+  val cells = vectorJson("cells")
+
+  val vector = 
+    if mtype == "matrix" then
+      cells.as[Seq[JsValue]].map(cell => JsonToStone(cell))
+    else
+      cells.as[Seq[JsValue]].map(cell => JsonToHStone(cell))
+  vector.toVector
+
+
+def JsonToGame(gameJson: JsValue): GameInterface =
+  val matrix = Matrix[Stone](gameJson("matrix").as[Seq[JsValue]].map(vector => JsonToVector(vector, "matrix")).toVector.asInstanceOf[Vector[Vector[Stone]]])
+  val hmatrix = Matrix[HStone](gameJson("hmatrix").as[Seq[JsValue]].map(vector => JsonToVector(vector, "hmatrix")).toVector.asInstanceOf[Vector[Vector[HStone]]])
+  val code = Code(JsonToVector(gameJson("code"), "matrix").asInstanceOf[Vector[Stone]])
+  val turn = gameJson("turn").as[Int]
+
+  Game(new Field(matrix, hmatrix), code, turn)
