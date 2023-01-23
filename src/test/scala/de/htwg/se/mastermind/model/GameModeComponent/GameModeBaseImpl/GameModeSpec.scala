eeb2c49ff9e941d13eacd4456cd7cd864f31bb16
Smokey95 <simon.publicmail@gmail.com>
2023-01-23 14:41:47 +0100

    Expand GameMode Specs

--- src/test/scala/de/htwg/se/mastermind/model/GameModeComponent/GameModeBaseImpl/GameModeSpec.scala
+++ src/test/scala/de/htwg/se/mastermind/model/GameModeComponent/GameModeBaseImpl/GameModeSpec.scala
@@ -12,32 +12,47 @@ package GameModeComponent.GameModeBaseImpl
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 import model.GameModeComponent.GameModeInterface
-import model.GameModeComponent.GameModeMockImpl.GameMode
+import model.GameModeComponent.GameModeBaseImpl.GameMode
 import model.GameComponent.GameBaseImpl.{Field, Stone, HintStone}
+import java.io.ByteArrayInputStream
+import java.io.StringReader
 
 //****************************************************************************** CLASS DEFINITION
 class GameModeSpec extends AnyWordSpec {
+  
   "A GameMode" should {
+    val string_easy   = "easy"
+    val string_medium = "medium"
+    val string_hard   = "hard"
+    val string_extrem = "extrem"
+    
+    val inputStreamEasy     = new StringReader(string_easy.stripMargin)
+    val inputStreamMedium   = new ByteArrayInputStream(string_medium.getBytes())
+    val inputStreamHard     = new ByteArrayInputStream(string_hard.getBytes())
+    val inputStreamExtrem   = new ByteArrayInputStream(string_extrem.getBytes())
+    
+    "have a parseInput method" in {
+      Console.withIn(inputStreamMedium){
+        GameMode.parseInput() should be (new Field(10, 4, Stone("E"), HintStone("E")))
+      }
+    }
+    
     "have a strategy for each difficulty" in {
       val strategy_easy   = GameMode.strategy_easy
       val strategy_medium = GameMode.strategy_medium
       val strategy_hard   = GameMode.strategy_hard
       val strategy_extrem = GameMode.strategy_extrem
-      //GameModeBaseImpl.GameMode.strategy_easy should not be (null)
+      
       strategy_easy       should not be (null)
       strategy_medium     should not be (null)
       strategy_hard       should not be (null)
       strategy_extrem     should not be (null)
       
-      strategy_easy       should be (new Field(1, 1, Stone("E"), HintStone("E")))
-      strategy_medium     should be (new Field(1, 1, Stone("E"), HintStone("E")))
-      strategy_hard       should be (new Field(1, 1, Stone("E"), HintStone("E")))
-      strategy_extrem     should be (new Field(1, 1, Stone("E"), HintStone("E")))
-    }
-    "have a parseInput method" in {
-      val parseInput = GameMode.parseInput()
-      parseInput should not be (null)
-      parseInput should be (new Field(1, 1, Stone("E"), HintStone("E")))
+      strategy_easy       should be (new Field(12, 4, Stone("E"), HintStone("E")))
+      strategy_medium     should be (new Field(10, 4, Stone("E"), HintStone("E")))
+      strategy_hard       should be (new Field(10, 5, Stone("E"), HintStone("E")))
+      strategy_extrem     should be (new Field(8,  5, Stone("E"), HintStone("E")))
+
     }
   }
 }
