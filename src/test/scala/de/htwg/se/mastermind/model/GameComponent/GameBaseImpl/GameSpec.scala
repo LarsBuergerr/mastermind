afc793b5916c2fd87f15ed179adc25a3b4fe01c1
lars buerger <lars.buerger@freenet.de>
2023-01-23 13:16:20 +0100

    added state equals and gamespec

--- src/test/scala/de/htwg/se/mastermind/model/GameComponent/GameBaseImpl/GameSpec.scala
+++ src/test/scala/de/htwg/se/mastermind/model/GameComponent/GameBaseImpl/GameSpec.scala
@@ -8,5 +8,19 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
 class GameSpec extends AnyWordSpec {
-  "A Game "
+  "A game is the playing field for the mastermind game." when {
+    "new" should {
+      val game = new Game()
+      "have a grid with 10 rows and 4 columns" in {
+        game.field.matrix.m.size should be(10)
+        game.field.matrix.m(0).length should be(4)
+      }
+      "should have the Init State" in {
+        game.state should be(Init())
+      }
+      "should have a maxTurn of 10" in {
+        game.getMaxTurns() should be(10)
+      }
+    }
+  }
 }
